9ac2c5a161a104f29a955a7e3bfd0b7a9b48391a
Ferid Memmedli <shahasist@gmail.com>
2021-03-27 14:45:17 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+int num1 = 100; 
+object obj1 = num1;
+
+
+print(num1.GetType());
+print(obj1.GetType());
+
+static void print(object value)
+{
+    Console.WriteLine(value);
+}
+
+static string input(string value)
+{
+    Console.Write(value);
+    return Console.ReadLine();
+}
