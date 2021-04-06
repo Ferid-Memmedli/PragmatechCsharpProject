0e7d33a5f470dcf68049f31d4b7df521b6d45169
Ferid Memmedli <shahasist@gmail.com>
2021-04-06 19:18:37 +0400

    Week4.Tasks add ...

---
+++ Week4.Task/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+#region Tasks
+
+#region 1. artan sira
+
+/*
+ * asagida verilen arrayin elementlerini artan sira ile
+ * ekrana cap edin
+ *
+ * qeyd : manual edilecek/ hazir methodlarsiz
+ */
+//int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
+
+#endregion 1. artan sira
+
+#region 2. azalan sira
+
+/*
+ * asagida verilen arrayin elementlerini azalan sira ile
+ * ekrana cap edin
+ *
+ * qeyd : manual edilecek/ hazir methodlarsiz
+ */
+//int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
+
+#endregion 2. azalan sira
+
+#region 3. geriden
+
+/*
+ * asagida verilen arrayin elementlerini tersine duzerek
+ * ekrana cap edin
+ *
+ * qeyd : manual edilecek/ hazir methodlarsiz
+ */
+//int[] arr = { 5, 6, 9, 12, 15, 7, 3, 20, 14, 36, 24 };
+
+#endregion 3. geriden
+
+#region 4. para pul el cirki
+
+/*
+ * istifadeci bankomatdan cixarmaq istediyi meblegi daxil edir(sonsuz sayda, emeliyyati dayandirmagida nezere alin).
+ * daxil edilen meblege uygun console-a eskinaslari sayi ile cap edirik.
+ *
+ * eskinaslar : 200, 100, 50, 20, 10, 5, 1
+ *
+ * meselen daxil edilen pul miqdari 45 dir:
+ * 2 eded 20 azn
+ * 1 eded 5 azn
+ *
+ */
+
+#endregion 4. para pul el cirki
+
+#region 5. tek ededlerin kublari cemi
+
+/*
+ * 1-10 qeder tek ededlerin kublarinin cemini yazin.
+ */
+
+#endregion 5. tek ededlerin kublari cemi
+
+#region 6. yas hesablanmasi
+
+/*
+ * dogum tarixini daxil eden istifedecinin
+ * yasini hesablayib ekrana cap edin
+ */
+
+#endregion 6. yas hesablanmasi
+
+#region 7.
+
+/*
+ * istifadeci terefinden daxil edilen iki musbet ededin hasilini vurma emeliyyati ile
+ * deyil toplama emeliyyati ile ekrana cap edin.
+ */
+
+#endregion 7.
+
+#endregion Tasks
+
+#region Methods
+
+static void Print(object value)
+{
+    Console.WriteLine(value);
+}
+
+static string Input(object value)
+{
+    Console.Write(value);
+    return Console.ReadLine();
+}
+
+#endregion Methods
