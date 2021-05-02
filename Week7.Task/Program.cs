79016dcd174bbacd6442f4de1d00928d40f17b90
Ferid Memmedli <shahasist@gmail.com>
2021-05-02 11:25:32 +0400

    Week7 add

---
+++ Week7.Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week7.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
